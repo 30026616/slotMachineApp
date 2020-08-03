a361d11e43b2f1d9dede71dc0d69204bb0ccfa0f
Ronald Magrata <30026616@student.toiohomai.ac.nz>
2020-08-04 10:08:06 +1200

    git init

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +23,25 @@ namespace SlotGameWorkshop
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        int dollars;
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void buttonAddCash_Click(object sender, RoutedEventArgs e)
+        {
+            dollars = dollars + 10;                             // Add $10 to the total
+            textBlockDollar.Text = "You have $" + dollars;      // Display the dollars
+        }
+
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        
+        {
+            imageWheel1.Source = new BitmapImage(new Uri("ms-appx:///Assets/win.png", UriKind.RelativeOrAbsolute));
+            imageWheel2.Source = new BitmapImage(new Uri("ms-appx:///Assets/win.png", UriKind.RelativeOrAbsolute));
+            imageWheel3.Source = new BitmapImage(new Uri("ms-appx:///Assets/win.png", UriKind.RelativeOrAbsolute));
+            imageWinLose.Source = new BitmapImage(new Uri("ms-appx:///Assets/WinGame.png", UriKind.RelativeOrAbsolute));
         }
     }
 }
